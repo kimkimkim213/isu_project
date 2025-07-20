107107462acd37236d8505c7c7aac70aee949838
kimkimkim213 <isug0521@gmail.com>
2025-07-20 23:09:41 +0900

    디버깅용콘솔로그만주궁장창추가한꼴
    컴포넌트도추가하고뭔가열심히했는데어째변화가없냐이런

---
+++ fe_project/fe_isu/src/components/ManageRecording.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="manage-recording">
+    <recorder-panel @recording-finished="handleRecordingComplete"></recorder-panel>
+    <past-meeting-list 
+      :recordings="recordings" 
+      @delete-recording="deleteRecording"
+      @update-filename="updateRecordingFilename">
+    </past-meeting-list>
+  </div>
+</template>
+
+<script>
+import RecorderPanel from './RecorderPanel.vue';
+import PastMeetingList from './PastMeetingList.vue';
+import { useRecordings } from '../conposable/ManageRecord.js';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+export default {
+  name: 'ManageRecording',
+  components: {
+    RecorderPanel,
+    PastMeetingList
+  },
+  setup() {
+    const { recordings, addRecording, deleteRecording, updateRecordingFilename } = useRecordings();
+    const error = ref(null);
+
+    const handleRecordingComplete = ({ audioBlob, filename, transcription }) => {
+      console.log('ManageRecording: recording-finished 이벤트 수신. 전사 결과:', transcription);
+      addRecording({
+        audioBlob: audioBlob,
+        filename: filename,
+        transcription: transcription
+      });
+    };
+
+    return {
+      recordings,
+      deleteRecording,
+      updateRecordingFilename,
+      handleRecordingComplete,
+      error
+    };
+  }
+};
+</script>
+
+<style scoped>
+.manage-recording {
+  padding: 20px;
+}
+</style>
